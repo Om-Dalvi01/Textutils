91aaf6967f67be3104edacd28f5d837396606341
omdalvi <omdalvi293@gmail.com>
2025-07-01 14:48:14 +0530

    Added reusable components and applied code changes

---
+++ src/components/TextForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function TextForm(props) {
+  return (
+    <>
+      <div>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+            <textarea className="form-control" id="mybox" rows="10"></textarea>
+        </div>
+        <button className="btn btn-primary">Convert to Uppercase</button>
+      </div>
+    </>
+  );
+}
